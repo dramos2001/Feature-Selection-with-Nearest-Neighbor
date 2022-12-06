0c965f5d9f6ef9cde4c0054ef45dae52a7d73e46
Damian Ramos <damianramos00@gmail.com>
2022-12-05 21:03:13 -0800

    Initial cross validation function added

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import random
 # import dataset
 data = pd.read_csv('Small_Data.txt', sep="  ", engine='python', header=None)
     
+# for testing purposes only
 def testing():
     row = data.loc[0]
 
@@ -17,10 +18,18 @@ def testing():
         print(data[0][i])
     
 
-def leave_one_out_cross_validation(data, current_set, feature_to_add):
+def crossValidationDemo():
+    for i in range(len(data)):
+        object_to_classify = data
+        label_object_to_classify = data[i][0]
+        
+        print("Looping over i, at the", i, "location")
+        print("The", i, "th object is in class", label_object_to_classify)
+
+def leaveOneOutCrossValidation(data, current_set, feature_to_add):
     return random.random()  # for testing purposes
     
-def feature_search():
+def featureSearch():
     current_features = []
     
     # for loop to traverse down search tree
@@ -35,7 +44,7 @@ def feature_search():
             # consider adding new feature if it has not been considered already
             if (j not in current_features):
                 print("--Considering adding the", row[j], "feature")
-                accuracy = leave_one_out_cross_validation(data, current_features, j+1)
+                accuracy = leaveOneOutCrossValidation(data, current_features, j+1)
                 
                 if (accuracy > best_accuracy):
                     best_accuracy = accuracy
@@ -44,5 +53,6 @@ def feature_search():
         current_features.append(feature_to_add)
         print("On level", i, "i added feature", feature_to_add, "to current set")
         
-
-feature_search()
+        
+# featureSearch()
+crossValidationDemo()
