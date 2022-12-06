cbc58eb3ad76ec313b12d28f3b24c07ac69fc1dd
Damian Ramos <damianramos00@gmail.com>
2022-12-05 22:55:15 -0800

    Updates to cross validation function

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import random
+import math
 
 # import dataset
 data = pd.read_csv('Small_Data.txt', sep="  ", engine='python', header=None)
@@ -7,6 +8,9 @@ data = pd.read_csv('Small_Data.txt', sep="  ", engine='python', header=None)
 # for testing purposes only
 def testing():
     row = data.loc[0]
+
+    # prints len of dataset i.e. number of rows
+    print(len(data))
 
     # testing how to iterate through dataset
     # prints first row of dataset
@@ -19,12 +23,35 @@ def testing():
     
 
 def crossValidationDemo():
+    correctly_classified = 0
+    
     for i in range(len(data)):
-        object_to_classify = data
-        label_object_to_classify = data[i][0]
+        row_to_classify = data.loc[i]
+        object_to_classify = []
+        for j in range(1, len(row_to_classify)):
+            object_to_classify.append(row_to_classify[j])
+        label_object_to_classify = data[0][i]
         
-        print("Looping over i, at the", i, "location")
-        print("The", i, "th object is in class", label_object_to_classify)
+        nearest_neighbor_distance = math.inf
+        nearest_neighbor_location = math.inf
+        
+        for k in range(len(data)):
+            print("Ask if", i, "is nearest neighbor with", k)
+            
+            if (k != i):
+                distance = math.sqrt(object_to_classify - data)  # fix this
+                if (distance < nearest_neighbor_distance):
+                    nearest_neighbor_distance = distance
+                    nearest_neighbor_location = k
+                    nearest_neighbor_label = data[nearest_neighbor_location][0]
+        
+        if (label_object_to_classify == nearest_neighbor_label):
+            correctly_classified += 1
+            
+        print("Object", i, "is class", label_object_to_classify)
+        print("Its nearest neighbor is", nearest_neighbor_location, "which is in class", nearest_neighbor_label)                    
+        # print("Looping over i, at the", i, "location")
+        # print("The", i, "th object is in class", label_object_to_classify)
 
 def leaveOneOutCrossValidation(data, current_set, feature_to_add):
     return random.random()  # for testing purposes
@@ -52,7 +79,6 @@ def featureSearch():
                     
         current_features.append(feature_to_add)
         print("On level", i, "i added feature", feature_to_add, "to current set")
-        
         
 # featureSearch()
 crossValidationDemo()
